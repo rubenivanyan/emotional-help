f6717d39d3ce7c6c2cb6a54f64d1daa7a5592e50
Taras Bilyk <tars.bilyk@gmail.com>
2021-12-11 14:28:11 +0200

    Added Book, BookDto, MaterialsDto, BookService, BookRepository, BookController

---
+++ PsychologicalAssistance.Core/Data/Helpers/AutoMapper/BookProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PsychologicalAssistance.Core.Data.DTOs;
+using PsychologicalAssistance.Core.Data.Entities;
+
+namespace PsychologicalAssistance.Core.Data.Helpers.AutoMapper
+{
+    public class BookProfile : Profile
+    {
+        public BookProfile()
+        {
+            CreateMap<Book, BookDto>().ReverseMap();
+        }
+    }
+}
---
+++ PsychologicalAssistance.Core/Repositories/Implementation/BookRepository.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using PsychologicalAssistance.Core.Data;
+using PsychologicalAssistance.Core.Data.DTOs;
+using PsychologicalAssistance.Core.Data.Entities;
+using PsychologicalAssistance.Core.Repositories.Abstract;
+using PsychologicalAssistance.Core.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PsychologicalAssistance.Core.Repositories.Implementation
+{
+    public class BookRepository : DataRepository<Book>, IBookRepository
+    {
+        private readonly IMapper _mapper;
+
+        public BookRepository(ApplicationDbContext dbContext, IMapper mapper) : base(dbContext)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<BookDto>> GetAllBooksDtoAsync()
+        {
+            var books = await GetAllItemsAsync();
+            if (books == null)
+            {
+                return null;
+            }
+
+            var booksDto = _mapper.Map<IEnumerable<Book>, IEnumerable<BookDto>>(books);
+            return booksDto;
+        }
+
+        public async Task<BookDto> GetBookByIdDtoAsync(int id)
+        {
+            var book = await GetItemByIdAsync(id);
+            if (book == null)
+            {
+                return null;
+            }
+
+            var bookDto = _mapper.Map<Book, BookDto>(book);
+            return bookDto;
+        }
+    }
+}
