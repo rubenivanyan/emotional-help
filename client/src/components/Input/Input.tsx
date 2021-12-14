f56a25cdfa2dc00c5880e3796feadfd82f2a61f5
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-12-15 01:53:34 +0300

    Simulate fetching

--- client/src/components/Input/Input.tsx
+++ client/src/components/Input/Input.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Input.scss';
 
-export const Input = ({ label }: { label: string }) => {
+export const Input = ({ label, isRequired }:
+  { label: string, isRequired?: boolean }) => {
   return (
     <div className="input-container">
       <label>{label}</label>
-      <input type="input" placeholder={'Type ' + label + ' here'}></input>
+      <input type="input"
+        placeholder={'Type ' + label + ' here'}
+        required
+      ></input>
     </div>
   );
 };
--- client/src/pages/TrainingAndConsultingPages/TrainingAndConsultingPages.tsx
+++ client/src/pages/TrainingAndConsultingPages/TrainingAndConsultingPages.tsx
@@ -1,5 +1,5 @@
 import { Block } from '../../components/Block/Block';
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useState } from 'react';
 import './TrainingAndConsultingPages.scss';
 import { BLOCK_TITLES } from '../../common/block-titles';
 import { TRAINING_AND_CONSULTING_TEXT } from '../../common/texts';
@@ -9,12 +9,18 @@ import { Input } from '../../components/Input/Input';
 
 const ParentComponent = ({ title, text, children }:
   PropsWithChildren<{ title: string, text: string }>) => {
-  // const [firstValue, setFirstValue] = useState('');
-  // const [secondValue, setSecondValue] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const [isSubmiting, setIsSubmiting] = useState(false);
 
   const handleSubmit = (event: React.FormEvent<HTMLElement>) => {
+    setIsSubmiting(true);
     event.preventDefault();
-    console.log('trying submit data...');
+    const random = Math.round(Math.random());
+    setTimeout(() => {
+      random ? setSuccess(true) : setError(true);
+      setIsSubmiting(false);
+    }, 2000);
   };
 
   return (
@@ -22,14 +28,43 @@ const ParentComponent = ({ title, text, children }:
       <Block title={title} percentWidth={100}>
         <div className={title + '-page-inner'}>
           <p>{text}</p>
-          <form onSubmit={(e) => handleSubmit(e)}>
-            {children}
-            <Button title={'submit'} type={BUTTON_TYPES.DEFAULT} />
-          </form>
-          <p className="attention">We will contact you as soon as possible</p>
+          {success ?
+            <Success /> :
+            error ?
+              <>
+                <Error />
+                <Button title={'retry'}
+                  type={BUTTON_TYPES.DEFAULT}
+                  onClick={() => setError(false)}
+                />
+              </> :
+              <form onSubmit={(e) => handleSubmit(e)}>
+                {children}
+                <Button
+                  title={'submit'}
+                  type={BUTTON_TYPES.DEFAULT}
+                  submiting={isSubmiting}
+                />
+              </form>
+          }
         </div>
       </Block>
     </section>
+  );
+};
+
+const Success = () => {
+  return (
+    <>
+      <h2>Done!</h2>
+      <p className="attention">We will contact you as soon as possible</p>
+    </>
+  );
+};
+
+const Error = () => {
+  return (
+    <h3>Something went wrong...</h3>
   );
 };
 
@@ -39,7 +74,7 @@ export const TrainingPage = () => {
       title={BLOCK_TITLES.TRAINING}
       text={TRAINING_AND_CONSULTING_TEXT.TRAINING_TEXT}
     >
-      <Input label={'Name'} />
+      <Input label={'Name'} isRequired={true} />
       <Input label={'E-mail'} />
     </ParentComponent>
   );
