c4ca8b716b785f551bc369f94472e6e88350ec5b
AnatoliyDubovtsev <anatoliy.dubovtsev01@gmail.com>
2021-12-15 12:05:42 +0600

    Added for Genres: seeding, controller, service and interface, repository and interface

---
+++ PsychologicalAssistance.Core/Repositories/Interfaces/IGenreRepository.cs
@@ -0,0 +1,13 @@
+﻿using PsychologicalAssistance.Core.Data.DTOs;
+using PsychologicalAssistance.Core.Data.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PsychologicalAssistance.Core.Repositories.Interfaces
+{
+    public interface IGenreRepository : IDataRepository<Genre>
+    {
+        Task<GenreDto> GetGenreByIdDtoAsync(int id);
+        Task<IEnumerable<GenreDto>> GetAllGenresDtoAsync();
+    }
+}
---
+++ PsychologicalAssistance.Services/Implementation/GenreService.cs
@@ -0,0 +1,27 @@
+﻿using PsychologicalAssistance.Core.Data.DTOs;
+using PsychologicalAssistance.Core.Data.Entities;
+using PsychologicalAssistance.Core.Repositories.Interfaces;
+using PsychologicalAssistance.Services.Abstract;
+using PsychologicalAssistance.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PsychologicalAssistance.Services.Implementation
+{
+    public class GenreService : BaseService<Genre>, IGenreService
+    {
+        private readonly IGenreRepository _genreRepository;
+
+        public GenreService(IDataRepository<Genre> dataRepository, IUnitOfWork unitOfWork, IGenreRepository genreRepository)
+            : base (dataRepository, unitOfWork)
+        {
+            _genreRepository = genreRepository;
+        }
+
+        public async Task<IEnumerable<GenreDto>> GetAllGenresAsync()
+            => await _genreRepository.GetAllGenresDtoAsync();
+
+        public async Task<GenreDto> GetGenreByIdAsync(int id)
+            => await _genreRepository.GetGenreByIdDtoAsync(id);
+    }
+}
