e77ca802a261cdbeb5160d7f56609726de1e5f3d
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-12-18 21:14:38 +0300

    A lot of styles, add mobile menu

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropsWithChildren, useState } from 'react';
 import './Header.scss';
 import { Logo } from '../Logo/Logo';
 import { Navbar } from '../Navbar/Navbar';
@@ -20,12 +20,29 @@ const SearchRegWrapper = () => {
   );
 };
 
+const NavigationWrapper = ({ children, isOpened }:
+  PropsWithChildren<{ isOpened?: boolean }>) => {
+  return (
+    <div className={'navigation-wrapper' + (isOpened ? ' opened' : '')}>
+      {children}
+    </div>
+  );
+};
+
 export const Header: React.FC = () => {
+  const [isOpened, setIsOpened] = useState(false);
+
   return (
     <header className="header">
       <Logo />
-      <Navbar />
-      <SearchRegWrapper />
+      <NavigationWrapper isOpened={isOpened}>
+        <Navbar />
+        <SearchRegWrapper />
+      </NavigationWrapper>
+      <button
+        className={'menu-opener' + (isOpened ? ' active' : '')}
+        onClick={() => setIsOpened(!isOpened)}
+      />
     </header>
   );
 };
