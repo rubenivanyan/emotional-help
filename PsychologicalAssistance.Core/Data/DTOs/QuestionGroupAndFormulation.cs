c60c8b9f62abd97bcc03ac39f01ebd1a7cc2ea14
AnatoliyDubovtsev <anatoliy.dubovtsev01@gmail.com>
2021-12-19 23:31:29 +0600

    Added external method for counting test results

---
+++ PsychologicalAssistance.Core/Data/DTOs/QuestionGroupAndFormulation.cs
@@ -0,0 +1,8 @@
+﻿namespace PsychologicalAssistance.Core.Data.DTOs
+{
+    public class QuestionGroupAndFormulation
+    {
+        public string Formulation { get; set; }
+        public string QuestionGroup { get; set; }
+    }
+}
---
+++ PsychologicalAssistance.Core/Repositories/Helpers/TestResultsCounting.cs
@@ -0,0 +1,46 @@
+﻿using PsychologicalAssistance.Core.Data.DTOs;
+using PsychologicalAssistance.Core.Data.Entities;
+using PsychologicalAssistance.Core.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace PsychologicalAssistance.Core.Repositories.Helpers
+{
+    public static class TestResultsCounting
+    {
+        public static List<QuestionGroupsValues> CountQuestionGroupsValues(TestResultsDto testResultsDto)
+        {
+            var questionGroupsValues = new List<QuestionGroupsValues>();
+            questionGroupsValues.Add(new QuestionGroupsValues
+            {
+                QuestionGroup = Enum.Parse<QuestionGroups>(testResultsDto.Questions[0].QuestionGroup),
+                Value = testResultsDto.ChosenVariants[0].Value
+            });
+
+            for (int i = 1; i < testResultsDto.ChosenVariants.Count; i++)
+            {
+                var group = Enum.Parse<QuestionGroups>(testResultsDto.Questions[i].QuestionGroup);
+                var isGroupAdded = false;
+                for (int j = 0; j < questionGroupsValues.Count; j++)
+                {
+                    if (questionGroupsValues[j].QuestionGroup == group)
+                    {
+                        questionGroupsValues[j].Value += testResultsDto.ChosenVariants[i].Value;
+                        isGroupAdded = true;
+                    }
+                }
+
+                if (!isGroupAdded)
+                {
+                    questionGroupsValues.Add(new QuestionGroupsValues
+                    {
+                        QuestionGroup = group,
+                        Value = testResultsDto.ChosenVariants[i].Value
+                    });
+                }
+            }
+
+            return questionGroupsValues;
+        }
+    }
+}
