083e6e800de2f5356a6f09ff3d8ba74a64af817f
AnatoliyDubovtsev <anatoliy.dubovtsev01@gmail.com>
2021-12-19 15:00:53 +0600

    Added InMemoryDatabaseCreation and basic test for ApplicationRepository

---
+++ PsychologicalAssistance.Tests/RepositoriesTests/ApplicationRepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using PsychologicalAssistance.Core.Data;
+using PsychologicalAssistance.Core.Data.DTOs;
+using PsychologicalAssistance.Core.Data.Helpers.AutoMapper;
+using PsychologicalAssistance.Core.Repositories.Implementation;
+using PsychologicalAssistance.Tests.Common;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PsychologicalAssistance.Tests.RepositoriesTests
+{
+    public class ApplicationRepositoryTests : InMemoryDatabaseCreation
+    {
+        private readonly IMapper _mapper;
+        public ApplicationRepositoryTests()
+        {
+            Setup();
+            _mapper = BasicClassForMocking.ConfigMapper(new ApplicationProfile());
+        }
+
+        [Fact]
+        public async Task GetAllApplicationsDtoAsync_ReturnsListOfApplicationDto()
+        {
+            //arrange
+            using (var dbContext = new ApplicationDbContext(dbContextOptions))
+            {
+                var repository = new ApplicationRepository(dbContext, _mapper);
+
+                //act
+                var result = await repository.GetAllApplicationsDtoAsync();
+
+                //assert
+                Assert.NotNull(result);
+                Assert.IsType<List<ApplicationDto>>(result);
+            }
+        }
+    }
+}
