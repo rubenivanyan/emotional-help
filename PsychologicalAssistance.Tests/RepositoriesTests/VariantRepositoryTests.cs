2c5b9d9d6ff6cff575e48fe0f678e3e6f64d82a7
Melnyk Nick <melnikmukola@gmail.com>
2021-12-20 05:14:04 +0200

    Create additional unit tests

---
+++ PsychologicalAssistance.Tests/RepositoriesTests/VariantRepositoryTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using PsychologicalAssistance.Core.Data;
+using PsychologicalAssistance.Core.Data.Entities;
+using PsychologicalAssistance.Core.Data.Helpers.AutoMapper;
+using PsychologicalAssistance.Core.Enums;
+using PsychologicalAssistance.Core.Repositories.Implementation;
+using PsychologicalAssistance.Tests.Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PsychologicalAssistance.Tests.RepositoriesTests
+{
+    public class VariantRepositoryTests : InMemoryDatabaseCreation
+    {
+        private readonly IMapper _mapper;
+        public VariantRepositoryTests()
+        {
+            Setup();
+            _mapper = BasicClassForMocking.ConfigMapper(new VariantProfile());
+        }
+
+        [Fact]
+        public async Task VariantRepository_GetAllVariants_ReturnAllValues()
+        {
+            using (var context = new ApplicationDbContext(dbContextOptions))
+            {
+                //arrange
+                var variantRepository = new VariantRepository(context, _mapper);
+
+                //act
+                var variants = await variantRepository.GetAllVariantsDtoAsync();
+
+                //assert
+                Assert.NotNull(variants.ToList());
+                Assert.Equal(8, variants.ToList().Count);
+                Assert.Equal("indifference", variants.Select(b => b.Formulation).First());
+
+            }
+        }
+    }
+}
