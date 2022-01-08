b02b2bedd8d9d21c87ced5970fbb3356f33a45d3
Vladimir Abdullaev <a.voxagamer@gmail.com>
2022-01-08 15:25:46 +0300

    Remove credentials apiGet

--- client/src/api/fetch/fetch.ts
+++ client/src/api/fetch/fetch.ts
@@ -22,6 +22,6 @@ export const apiFetchPut = (path: string, item: any) => {
 
 export const apiFetchGet = async (path: string) => {
   return await fetch(PATH + path, {
-    credentials: 'include',
+    // credentials: 'include',
   });
 };
