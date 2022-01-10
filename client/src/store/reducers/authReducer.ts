eb266965ea0bddbe7ca79d0c70c82db6bb5a7ee0
Vladimir Abdullaev <a.voxagamer@gmail.com>
2022-01-10 09:59:41 +0300

    Add console logs

--- client/src/store/reducers/authReducer.ts
+++ client/src/store/reducers/authReducer.ts
@@ -5,13 +5,15 @@ const initState = {
 };
 
 export const authReducer = (state = initState, action) => {
+  console.log('action ', action);
+  console.log('action type ', action.type);
+  console.log('state ', state);
   switch (action.type) {
-    case AUTH_FETCH_SUCCEEDED: {
+    case AUTH_FETCH_SUCCEEDED:
       const auth = action.payload.data;
       return {
         isLogged: auth,
       };
-    }
     default:
       return state;
   }
